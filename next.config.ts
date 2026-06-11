f047e6fa3f3bd6a612ac64dd3311543d989d848e
LoDRZin <roger.derik211@gmail.com>
2026-06-11 19:34:24 -0300

    chore: ignore typescript and eslint during vercel build

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
       { hostname: "ufs.sh" },
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
